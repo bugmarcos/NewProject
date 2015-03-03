527f3bae65485b16c25eea2aaf2edbb34aba19a8
Vijay Patel <vijaypatelmca049@gmail.com>
2015-03-02 21:40:38 -0800

    Icon added... for testing...

--- app/src/main/java/com/foodapp/foodnearme/SplashActivity.java
+++ app/src/main/java/com/foodapp/foodnearme/SplashActivity.java
@@ -42,7 +42,6 @@ public class SplashActivity extends ActionBarActivity implements LoginFrag.OnFra
         }, 1000);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
